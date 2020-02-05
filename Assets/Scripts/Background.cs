3aa90643cc950a1f4ce33f24f43c9c999239873c
mircoNameless <xishu@hoosho.com>
2020-02-05 15:51:07 +0800

    feat:提交项目基础，背景滚动和顶部刺

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public Material material;
+    public Vector2 speed;
+    
+    
+    public void Update()
+    {
+        material.mainTextureOffset += speed * Time.deltaTime;
+    }
+}
